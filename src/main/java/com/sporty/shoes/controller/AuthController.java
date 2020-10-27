959e6905f913392724f87eb79b99d4e1ae150c89
Deepak Kumar <deepak.k25101997@gmail.com>
2020-10-27 18:33:12 +0530

    jenkins pipeline updated

--- src/main/java/com/sporty/shoes/controller/AuthController.java
+++ src/main/java/com/sporty/shoes/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.sporty.shoes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,11 @@ public class AuthController {
 	@PostMapping("/logout")
 	public String deAuthenticate(@RequestHeader("token") String token) throws MyAuthException {
 		return authService.logout(token);
+	}
+	
+	@GetMapping("/welcome")
+	public String welcomeMessage() {
+		return "Hello";
 	}
 	
 }
